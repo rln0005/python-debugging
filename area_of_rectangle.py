c2ab4ff6aad7f330afa69142b1dc16d9dc37e6ac
Becca <rln0005@auburn.edu>
2021-03-31 10:10:58 -0700

    area_of_rectangle.py fxn added back in

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -12,7 +12,7 @@ def area_of_rectangle(height, width = None):
 
     Parameters
     ----------
-    height : int or float 
+    height : int or float
         The height of the rectangle.
     width : int or float
         The width of the rectangle. If `None` width is assumed to be equal to 
@@ -31,6 +31,15 @@ def area_of_rectangle(height, width = None):
     14
     """
 
+    if width == None:
+        width = height
+    area = height * width
+    message = "The area of a {h} X {w} rectangle is {a}".format(
+        h = height,
+        w = width,
+        a = area)
+    print(message)
+
 if __name__ == '__main__':
     if (len(sys.argv) < 2) or (len(sys.argv) > 3):
         message = (
@@ -38,19 +47,11 @@ if __name__ == '__main__':
                 "\tthe height of a square or the height and width of a "
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-
     height = sys.argv[1]
+    height = float(height)
     if len(sys.argv) < 3:
         width = height
     else:
         width = sys.argv[2]
-
-    height = int(height)
-    width = int(width)
-    area = height * width
-
-    message = "The area of a {h} X {w} rectangle is {a}".format(
-            h = height,
-            w = width,
-            a = area)
-    print(message)
+        width = float(width)
+    area_of_rectangle(height, width)
